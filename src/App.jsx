e709ca81729f41a8f7e2cbaaf0f19f4a9a346a34
jaiebobade/ <jaiebobade26@gmail.com>
2024-04-02 13:10:15 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Manager from './components/Manager'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    <div className='bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>   
+      {/* <div>my ui</div> */}
+
+      {/* <div className='min-h-[87vh]'> */}
+
+      <Manager />  
+
+      </div>
+
+      
+
+      {/* </div> */}
+
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    
+
+    <div className='bg-slate-800 text-white flex flex-col justify-center items-center w-full'>
+
+
+<div className="logo font-bold text-white text-2xl">
+          <span className='text-green-500'> &lt; </span>
+          <span>Pass</span>
+          <span className='text-green-500'>OP /&gt; </span>
+        </div>
+
+
+      <div className='flex justify-center items-center'>
+      created with <img className='w-7 mx-2' src="icons/yes.png" alt="heart"/> by CodeWithJaie
+      </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Footer
